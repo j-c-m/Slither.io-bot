d20eece614e49b3687bcceecf46969bf2d96e218
Jesse Miller <jmiller@jmiller.com>
2016-05-17 22:43:13 -0500

    Fix typo - 1.5.1

--- bot.user.js
+++ bot.user.js
@@ -7,7 +7,7 @@ The MIT License (MIT)
 // ==UserScript==
 // @name         Slither.io-bot
 // @namespace    https://github.com/j-c-m/Slither.io-bot
-// @version      1.5.0
+// @version      1.5.1
 // @description  Slither.io bot
 // @author       Jesse Miller
 // @match        http://slither.io/
@@ -1052,7 +1052,7 @@ var userInterface = (function() {
     if (window.mobileRender) {
         userInterface.toggleMobileRendering(true);
     } else {
-        userInterface.toggleMobileRenderin(false);
+        userInterface.toggleMobileRendering(false);
     }
 
     // Unblocks all skins without the need for FB sharing.
