bdc4462bf0973655243da5b1cc13612c246277a7
Jesse Miller <jmiller@jmiller.com>
2016-05-19 15:07:03 -0500

    Version 1.6.6 - minor fastAtan2 fix.

--- bot.user.js
+++ bot.user.js
@@ -7,7 +7,7 @@ The MIT License (MIT)
 // ==UserScript==
 // @name         Slither.io Bot Championship Edition
 // @namespace    https://github.com/j-c-m/Slither.io-bot
-// @version      1.6.5
+// @version      1.6.6
 // @description  Slither.io Bot Championship Edition
 // @author       Jesse Miller
 // @match        http://slither.io/
