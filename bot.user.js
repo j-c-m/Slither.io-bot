72a7a7e643a27cf66d80c00f41cb878cd3929fd9
Jesse Miller <jmiller@jmiller.com>
2016-05-18 19:14:38 -0500

    Version 1.6.0 - Optimized food logic.

--- bot.user.js
+++ bot.user.js
@@ -7,7 +7,7 @@ The MIT License (MIT)
 // ==UserScript==
 // @name         Slither.io-bot
 // @namespace    https://github.com/j-c-m/Slither.io-bot
-// @version      1.5.1
+// @version      1.6.0
 // @description  Slither.io bot
 // @author       Jesse Miller
 // @match        http://slither.io/
