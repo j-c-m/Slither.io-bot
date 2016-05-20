4121f5b0c69c6fbfdbed3705008e81787c1fb850
Jesse Miller <jmiller@jmiller.com>
2016-05-19 21:59:02 -0500

    Version 1.6.7 - Bigger head detection circle.

--- bot.user.js
+++ bot.user.js
@@ -7,7 +7,7 @@ The MIT License (MIT)
 // ==UserScript==
 // @name         Slither.io Bot Championship Edition
 // @namespace    https://github.com/j-c-m/Slither.io-bot
-// @version      1.6.6
+// @version      1.6.7
 // @description  Slither.io Bot Championship Edition
 // @author       Jesse Miller
 // @match        http://slither.io/
