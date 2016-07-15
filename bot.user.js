8ff1a7c5c27870377590e988c79feecd6a4925af
Jesse Miller <jmiller@jmiller.com>
2016-07-15 11:56:45 -0500

    Version 3.0.2

--- bot.user.js
+++ bot.user.js
@@ -8,7 +8,7 @@ The MIT License (MIT)
 // ==UserScript==
 // @name         Slither.io Bot Championship Edition
 // @namespace    https://github.com/j-c-m/Slither.io-bot
-// @version      3.0.1
+// @version      3.0.2
 // @description  Slither.io Bot Championship Edition
 // @author       Jesse Miller
 // @match        http://slither.io/
